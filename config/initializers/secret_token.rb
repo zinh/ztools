1053123fe988df48d94e526cfdcf4bbf914814f5
Vinh Nguyen <xuanvinhct@gmail.com>
2014-02-02 07:16:16 +0000

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZinhTool::Application.config.secret_key_base = 'b7c1289cca9e2665fc91d69a19c9b0e8147283d1e49487220749edf812b0c9117384db5883838b76282ec18a62257c89b9352ff118671d2d1e6dece6176664e8'
